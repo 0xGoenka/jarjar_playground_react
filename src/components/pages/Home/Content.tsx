cd3c04e2386d385d4197d04e4f99a772abfb1879
Esteban LECLET <estebanleclet@gmail.com>
2024-08-17 22:34:26 +0300

    add userminted nft section

--- src/components/pages/Home/Content.tsx
+++ src/components/pages/Home/Content.tsx
@@ -99,6 +99,38 @@ export const Content = () => {
           and receiving rev share rewards from oracles fees.
         </p>
       </section>
+
+      <h2 className="nft-section-title mt-12 ">Your minted Eggs</h2>
+      <Carousel
+        className="w-full sm:max-w-screen-lg max-w-[300px] nft-grid overflow-visible"
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 5000,
+          }),
+        ]}
+      >
+        <CarouselContent className="p-4">
+          {arrNft.map((nft, key) => (
+            <CarouselItem
+              className="first-child:pl-0 p-2 md:basis-1/2 lg:basis-1/3 sm:basis-1/1"
+              key={key}
+            >
+              <div className="nft-item">
+                <img src={nft.url} alt="JarJar NFT 1" />
+                <div className="nft-info">
+                  <h3>EGGS #000{key + 1}</h3>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
